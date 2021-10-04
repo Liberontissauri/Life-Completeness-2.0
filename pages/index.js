18242f83fd851aa2f0d065358072789f238d9939
Liberontissauri <jp.liberon@gmail.com>
2021-10-04 10:02:23 +0100

    added navbar and background to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import LifeNavbar from '@/components/LifeNavbar'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <LifeNavbar variant="homepage"></LifeNavbar>
+      
+      <img src="/background.jpg" className={styles.background}>
+      </img>
+      <div className={styles.backgroundOverlay}></div>
+    </div>
   )
 }
