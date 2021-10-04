9f40cdde8ddf46189495c28b484696568aa55ada
Liberontissauri <jp.liberon@gmail.com>
2021-10-04 11:36:30 +0100

    added a container class to center the contents on the homepage (last one was not complete)

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <LifeNavbar variant="homepage"></LifeNavbar>
       
       <img src="/background.jpg" className={styles.background}>
