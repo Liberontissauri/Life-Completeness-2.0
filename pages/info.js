4cda43a9542550788a68bc1ab5fc994d5ca98e90
Liberontissauri <jp.liberon@gmail.com>
2021-10-05 14:22:20 +0100

    added info page

--- pages/info.js
+++ pages/info.js
@@ -1,10 +1,15 @@
+import LifeButton from '@/components/LifeButton'
 import LifeNavbar from '@/components/LifeNavbar'
 import React from 'react'
+import styles from "@/styles/Info.module.css"
 
 function info() {
     return (
-        <div>
+        <div className={styles.container}>
             <LifeNavbar variant="quiz"></LifeNavbar>
+            <h1 className={styles.title}>Life Completeness</h1>
+            <h2 className={styles.subTitle}>Life completeness is an app to see how much of your life you have possibly lived. We will now ask you some questions to better reach this goal, are you ready?</h2>
+            <LifeButton text="Yeah!" variant="dark"></LifeButton>
         </div>
     )
 }
