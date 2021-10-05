7c1f340e7b5e20ec69c39fdee66c6d70d8874424
Liberontissauri <jp.liberon@gmail.com>
2021-10-05 16:04:22 +0100

    added input component

---
+++ components/LifeInput/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./style.module.css"
+
+function LifeInput(props) {
+    function changeLetter(e) {
+        const input = e.key;
+        const number_check_regex = new RegExp("^[0-9]+$");
+        if(!number_check_regex.test(input)) e.preventDefault()
+    }
+    return (
+        <div style={{margin: props.margin}}>
+            <input className={styles.input} onKeyPress={changeLetter}></input>
+        </div>
+    )
+}
+
+export default LifeInput
