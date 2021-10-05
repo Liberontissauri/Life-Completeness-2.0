2411aee6c4cae8cd5ef2f91ac2cd9b5c56e094c4
Liberontissauri <jp.liberon@gmail.com>
2021-10-05 15:21:41 +0100

    added sex selection page

--- pages/info.js
+++ pages/info.js
@@ -2,16 +2,18 @@ import LifeButton from '@/components/LifeButton'
 import LifeNavbar from '@/components/LifeNavbar'
 import React from 'react'
 import styles from "@/styles/Info.module.css"
+import { useRouter } from 'next/router'
 
-function info() {
+function Info() {
+    const router = useRouter();
     return (
         <div className={styles.container}>
             <LifeNavbar variant="quiz"></LifeNavbar>
             <h1 className={styles.title}>Life Completeness</h1>
             <h2 className={styles.subTitle}>Life completeness is an app to see how much of your life you have possibly lived. We will now ask you some questions to better reach this goal, are you ready?</h2>
-            <LifeButton text="Yeah!" variant="dark"></LifeButton>
+            <LifeButton text="Yeah!" variant="dark" onClick={e => router.push("/sexSelection")}></LifeButton>
         </div>
     )
 }
 
-export default info
+export default Info
---
+++ pages/sexSelection.js
@@ -0,0 +1,29 @@
+import LifeButton from '@/components/LifeButton'
+import LifeNavbar from '@/components/LifeNavbar'
+import React from 'react'
+import styles from "@/styles/SexSelection.module.css"
+import LifeButtonIcon from '@/components/LifeButtonIcon'
+import { useRouter } from 'next/router'
+
+function SexSelection() {
+    const router = useRouter();
+    function selectMale() {
+        router.push("/ageSelection")
+    }
+    function selectFemale() {
+        router.push("/ageSelection")
+    }
+    return (
+        <div className={styles.container}>
+            <LifeNavbar variant="quiz"></LifeNavbar>
+            <h1 className={styles.title}>Choose a sex</h1>
+            <h2 className={styles.subTitle}>We need this information because life expectancy data is different for men and women</h2>
+            <div className={styles.buttonsDiv}>
+                <LifeButtonIcon img="/icons/Male.svg" margin="0 0.75rem" onClick={selectMale}></LifeButtonIcon>
+                <LifeButtonIcon img="/icons/Female.svg" margin="0 0.75rem" onClick={selectFemale}></LifeButtonIcon>
+            </div>
+        </div>
+    )
+}
+
+export default SexSelection
